44ef5c6d9e5fb66638d91f2836d0129e88a9e713
nghia.ngo <nghia.ngo.22.11@gmail.com>
2019-09-28 02:25:13 +0700

    Add json-replace component

--- src/app/pages/dash-board/dash-board.component.ts
+++ src/app/pages/dash-board/dash-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router, Routes} from '@angular/router';
 
 @Component({
   selector: 'json-dash-board',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashBoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  navigateTo(replaceJson: string) {
+    this.route.navigate(['dashboard/json-replace']);
+  }
 }
