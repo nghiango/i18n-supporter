297ac5914adaf866bf4001b863cc5d13b67ea56d
Nghia Ngo <nghia.ngo@ubitec.com>
2019-11-24 13:40:50 +0700

    Add dialog to add new key

--- src/app/components/add-key-dialog/add-key-dialog.component.ts
+++ src/app/components/add-key-dialog/add-key-dialog.component.ts
@@ -7,7 +7,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
   styleUrls: ['./add-key-dialog.component.scss']
 })
 export class AddKeyDialogComponent implements OnInit {
-
+  public files = ['en.json', 'de.json', 'fr.json'];
   constructor(
     public dialogRef: MatDialogRef<AddKeyDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: string
@@ -16,4 +16,11 @@ export class AddKeyDialogComponent implements OnInit {
   ngOnInit() {
   }
 
+  closeDialog() {
+
+  }
+
+  addKey() {
+
+  }
 }
